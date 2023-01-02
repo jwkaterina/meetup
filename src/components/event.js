49d24507334b0d318ec4df1484e3c88f1eff779d
Kateryna Logoshko <103837638+jwkaterina@users.noreply.github.com>
2023-01-02 14:43:12 +0200

    feat(24): Add double digit participant option (#28)

--- src/components/event.js
+++ src/components/event.js
@@ -59,6 +59,7 @@ export default class Event {
             document.getElementById(`${this.id}`).remove();
             return;
         }
+
         let eventSlot;
         if (document.getElementById(`${this.id}`)) {
             eventSlot = document.getElementById(`${this.id}`);
@@ -71,42 +72,33 @@ export default class Event {
             });
         }
 
-        let lis = "";
-        this.names.forEach((value, index) => {
-            lis += `<li class="member" member=${index + 1}>${value}</li>`
-        });
-        let txt = "";
-        txt = `<a class="place" target="_blank">${this.place}</a>
-            <ol class="list">${lis}</ol>`
-        eventSlot.innerHTML = txt;
         if (media.matches) {
+            const h = this.slotHeightMobile;
+            eventSlot.style.top = (this.startHour + this.startMinutes / 60 ) * h + 1 + "px";
+            eventSlot.style.bottom = (24 - this.endHour + this.endMinutes / 60) * h + 3 + "px";
+
             if(this.names.length == 0) {
                 return
             } else {
                 const circle = document.createElement("div");
                 circle.className = "circle";
                 circle.innerHTML = this.names.length;
-                eventSlot.innerHTML = "";
                 eventSlot.appendChild(circle);
             }
-        }
-
-        let h;
-        if (media.matches) {
-            h = this.slotHeightMobile;
-            eventSlot.style.top = (this.startHour + this.startMinutes / 60 ) * h + 1 + "px";
-            eventSlot.style.bottom = (24 - this.endHour + this.endMinutes / 60) * h + 3 + "px";
-            // console.log(h);
         } else {
-            h = this.slotHeight;
+            const h = this.slotHeight;
             eventSlot.style.top = (this.startHour + this.startMinutes / 60 ) * h + 1 + "px";
             eventSlot.style.bottom = (24 - this.endHour + this.endMinutes / 60) * h + 5 + "px";
-            // console.log(h);
+
+            let lis = "";
+            this.names.forEach((value, index) => {
+                lis += `<li class="member" member=${index + 1}>${value}</li>`
+            });
+            let txt = "";
+            txt = `<a class="place" target="_blank">${this.place}</a>
+                <ol class="list">${lis}</ol>`
+            eventSlot.innerHTML = txt;
         }
-        // const h = document.querySelector(".slot").offsetWidth
-        // eventSlot.style.top = (this.startHour + this.startMinutes / 60 - this.settings.dayStarts) * h -+ 1 + "px";
-        // eventSlot.style.bottom = (this.settings.dayEnds - this.endHour + this.endMinutes / 60) * h + 5 + "px";
-    
 
         if(this.names.includes(ctx.principal.userName)) {
             this.color = "var(--blue)";
