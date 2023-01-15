cbcd3ba8b0e1c0540537c49c0ed73fa90bb46f1e
Kateryna Logoshko <jwkaterina@gmail.com>
2023-01-15 12:54:43 +0100

    Add scroll to select list

--- src/component/editors_list.js
+++ src/component/editors_list.js
@@ -5,6 +5,13 @@ export const editors = [
     {name: "Wolfgang Schröder", id:"4"},
     {name: "Ives Banga", id:"5"},
     {name: "Johnathan Hesse", id:"6"},
-    {name: "Antony Cramer", id:"7"},
+    {name: "Antony Cramer", id:"8"},
+    {name: "Antony Cramer", id:"9"},
+    {name: "Antony Cramer", id:"10"},
+    {name: "Antony Cramer", id:"11"},
+    {name: "Antony Cramer", id:"12"},
+    {name: "Antony Cramer", id:"13"},
+    {name: "Antony Cramer", id:"14"},
+    {name: "Antony Cramer", id:"15"},
     {name: "Kateryna Logoshko", id:"369e16e1-edc1-4418-ba6c-a872059540b1"}
 ]
