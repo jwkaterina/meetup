53520a0c15a23244a55105046ebfb61900cb394d
Kateryna Logoshko <103837638+jwkaterina@users.noreply.github.com>
2023-02-08 13:50:27 +0200

    feat(106): change modals position to fixed (#107)

--- src/component/intro-modal.js
+++ src/component/intro-modal.js
@@ -10,7 +10,6 @@ export default class IntroModal {
     show() {
         document.body.appendChild(this.clon);
 
-        scroll(0, 0);
         document.body.classList.add("opaque");
         document.body.style.overflow = 'hidden';
         const container = document.getElementById("introModal");
